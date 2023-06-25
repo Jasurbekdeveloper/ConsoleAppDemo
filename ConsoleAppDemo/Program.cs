ce4965ad840342d0749d9a8336af07e20d53d2b7
Jasurbek <mamatqulovjasurbek805@gmail.com>
2023-06-25 16:16:20 +0500

    Add project files.

---
+++ ConsoleAppDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleAppDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
